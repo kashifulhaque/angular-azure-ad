e7f7c74ea162df9070b4bda2419bddb80b99285d
kashifulhaque <haque.kashiful7@gmail.com>
2022-08-31 19:35:13 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,56 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+// import { NgModule } from '@angular/core';
+// import { Routes, RouterModule } from '@angular/router';
+// import { HomeComponent } from './home/home.component';
+// import { ProfileComponent } from './profile/profile.component';
+// import { MsalGuard } from '@azure/msal-angular';
+// import { BrowserUtils } from '@azure/msal-browser';
 
-const routes: Routes = [];
+// const routes: Routes = [
+//   {
+//     path: 'profile',
+//     component: ProfileComponent,
+//     canActivate: [MsalGuard]
+//   },
+//   {
+//     path: '',
+//     component: HomeComponent
+//   },
+// ];
+
+// const isIframe = window !== window.parent && !window.opener;
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes, {
+//     initialNavigation: !BrowserUtils.isInIframe() && !BrowserUtils.isInPopup() ? 'enabledNonBlocking' : 'disabled' // Don't perform initial navigation in iframes
+//   })],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
+
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { MsalGuard } from '@azure/msal-angular';
+
+const routes: Routes = [
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    canActivate: [MsalGuard]
+  },
+  {
+    path: '',
+    component: HomeComponent
+  },
+];
+
+const isIframe = window !== window.parent && !window.opener;
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    initialNavigation: !isIframe ? "enabledBlocking" : "disabled" // Don't perform initial navigation in iframes
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
